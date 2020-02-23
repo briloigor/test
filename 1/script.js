795c0e24dec510a866fdb28b0fd72a37dfadb13f
igor <briloigor@ukr.net>
2020-02-23 16:50:41 +0200

    Это наш первый комит

---
+++ 1/script.js
@@ -0,0 +1,45 @@
+//'use strict';
+
+a= 5;
+console.log(a)
+
+/*console.log(4/0);
+
+let something;
+console.log(something);
+
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(person.name);
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert('hello word');
+
+//let answer = confirm("are you here?");      
+
+let answer = prompt("Есть ли 18?","da");
+console.log(typeof(arr)); 
+*/
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log(+ "2" + 2);
+
+let isChecked = false,
+    isClose = true;
+
+console.log(isChecked || isClose);  
